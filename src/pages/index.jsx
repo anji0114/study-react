72bea5fe6b5530d63d507803afb4d1c1c668b913
anji0114 <anji.tnk.140114@gmail.com>
2022-03-27 13:23:36 +0900

    useCallbackの削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,15 @@
 import Head from 'next/head'
-import { useCallback } from 'react';
+// import { useCallback } from 'react';
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main'
 import styles from 'src/styles/Home.module.css'
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
+  const handleClick =(e) => {
     console.log(e.target.href);
     e.preventDefault();
-  }, []);
+  };
 
 
   return (
