96c3a1cf032fda14aac3b419cc5e594c6d4e0a26
anji0114 <anji.tnk.140114@gmail.com>
2022-03-27 13:16:29 +0900

    不要な記述、試しのクリックイベント

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,23 @@
 import Head from 'next/head'
+import { useCallback } from 'react';
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main'
 import styles from 'src/styles/Home.module.css'
 
+// const handleClick = (e, foo) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+//   alert(foo)
+// };
+
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+  }, []);
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +25,14 @@ export default function Home() {
       </Head>
 
       <Header />
+      <div style={{ textAlign: "center" }}>
+        <a
+          href='/about'
+          onClick={handleClick}
+        >
+          ボタン
+        </a>
+      </div>
       <Main page="index" />
 
       <Footer />
