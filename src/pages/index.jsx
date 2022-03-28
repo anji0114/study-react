508e761ac18c850a23552187f16ad16ff9863116
anji0114 <anji.tnk.140114@gmail.com>
2022-03-28 11:46:34 +0900

    ✨ useEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,27 @@
 import Head from 'next/head'
-import { useCallback } from 'react';
+import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main'
-import styles from 'src/styles/Home.module.css'
+import { useCallback } from 'react';
+import { useEffect } from 'react';
+
 
 export default function Home() {
   const handleClick = useCallback((e) => {
-    console.log(e.target.href);
+    console.log(e.target);
     e.preventDefault();
   }, []);
 
+  useEffect(() => {
+    console.log("マウント時")
+    document.body.style.backgroundColor = 'lightblue';
+
+    return () => {
+      console.log("アンマウント時")
+      document.body.style.backgroundColor = '';
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
