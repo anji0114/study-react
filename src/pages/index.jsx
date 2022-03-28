025d1942009ce1a2e44d4e86f7771a38aa33407d
anji0114 <anji.tnk.140114@gmail.com>
2022-03-28 16:06:12 +0900

    ✨ useStateで文字列と真偽値について

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,13 +8,27 @@ import { useState } from 'react';
 import { useCallback } from 'react';
 
 export default function Home() {
-  const [count, setCount] = useState(1)
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback((e) => {
+  const handleClick = useCallback(() => {
     if (count < 10) {
       setCount(count => count + 1);
     }
   }, [count]);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
@@ -23,21 +37,25 @@ export default function Home() {
     };
   }, []);
 
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
+      <Head><title>Index Page</title></Head>
 
       <Header />
       <div style={{ textAlign: "center" }}>
-        <h1>{count}</h1>
-        <button onClick={handleClick} >
-          ボタン
-        </button>
+        {isShow ? <h1>{count}</h1> : null}
+        <button onClick={handleClick} >ボタン</button><br />
+        <button onClick={handleDisplay}>
+          {isShow ? "非表示" : "表示"}
+        </button><br />
+        <input
+          type="text"
+          value={text}
+          onChange={handleChange}
+        />
       </div>
       <Main page="index" />
-
       <Footer />
     </div>
   );
