e922e35d238cd6b6505ff1aadf6cd329a60b5eb0
anji0114 <anji.tnk.140114@gmail.com>
2022-04-06 11:59:22 +0900

    ✨ APIの取得

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,40 @@
-import Head from 'next/head'
-import styles from 'src/styles/Home.module.css'
-import { Footer } from 'src/components/Footer'
-import { Header } from 'src/components/Header';
-import { Main } from 'src/components/Main'
+import Head from "next/head";
+import styles from "src/styles/Home.module.css";
+import { Header } from "src/components/Header";
+import { useState, useCallback, useEffect } from "react";
 
 // function home
 const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  console.log(posts);
+
   return (
     <div className={styles.container}>
-      <Head><title>Index Page</title></Head>
+      <Head>
+        <title>Index Page</title>
+      </Head>
 
       <Header />
-      test
+      {posts.length > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ol>
+      ) : null}
     </div>
   );
-}
+};
 
 export default Home;
