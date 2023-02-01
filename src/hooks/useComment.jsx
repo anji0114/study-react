0fa0118c6ae0d92ebf6c9785e992243a8e77e9e9
anji0114 <anji.tnk.140114@gmail.com>
2023-02-02 06:43:43 +0900

    ⚡️ ISRを試すためにrevalidateを設定しました。

--- src/hooks/useComment.jsx
+++ src/hooks/useComment.jsx
@@ -1,13 +1,11 @@
 import { useRouter } from "next/router";
 import { API_URL } from "src/utils/const";
-import useSWR from "swr";
+import useSWRImmutable from "swr/immutable";
 
 export const useComment = () => {
   const router = useRouter();
-  const { data, error } = useSWR(
-    router.query.id
-      ? `${API_URL}/comments/${router.query.id}`
-      : null
+  const { data, error } = useSWRImmutable(
+    router.query.id ? `${API_URL}/comments/${router.query.id}` : null
   );
 
   return {
