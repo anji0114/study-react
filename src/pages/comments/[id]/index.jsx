9f0318d4926ebb1f2d90006c97c5fa695f31e84d
anji0114 <anji.tnk.140114@gmail.com>
2023-02-01 12:39:31 +0900

    ⚡️ コメントの個別ページをSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 import { CommentComponent } from "src/components/Comment";
 import { Header } from "src/components/Header";
+import { SWRConfig } from "swr";
 
-const CommentId = () => {
+export const getStaticPaths = async () => {
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+
+  const commentsData = await comments.json();
+  const paths = commentsData.map((comment) => ({
+    params: { id: comment.id.toString() },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.params;
+  const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comment = await fetch(COMMENT_API_URL);
+  const commentsData = await comment.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENT_API_URL]: commentsData,
+      },
+    },
+  };
+};
+
+const CommentId = (props) => {
+  const { fallback } = props;
+
   return (
     <div>
       <Header />
-      <CommentComponent />
+      <SWRConfig value={{ fallback }}>
+        <CommentComponent />
+      </SWRConfig>
     </div>
   );
 };
